299ab6729e08ab8d9c70e7f99c9ab03b02a5ca68
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-03-14 06:08:24 +0500

    make a code of set and clear bit

--- Bit.java
+++ Bit.java
@@ -1,17 +1,41 @@
+package learningJava07;
+
 public class Bit {
     public static void main(String[] args){
-        // Set Bit
-        int n =5;
-        int position =2;
-        int bitMask= 1<<position;
-        if( (bitMask & n)==0){
-            System.out.println("the number is 0");
-        }
-            else
-            {
-                System.out.println("number is 1");
-            }
+        // // Set Bit
+        // int n =5;
+        // int position =2;
+        // int bitMask= 1<<position;
+        // if( (bitMask & n)==0){
+        //     System.out.println("the number is 0");
+        // }
+        //     else
+        //     {
+        //         System.out.println("number is 1");
+        //     }
+
+
+
+
+
+
+        //Set Bit
+        // int n =5;
+        // int pos=1;
+        // int bitMask = 1<<pos;
+        // int newNumber = bitMask | n;
+        // System.out.println(newNumber);
         
+
+
+
+        // Clear Bit
+        // int n =5;
+        // int posi =2;
+        // int bitMask = 1<<posi;
+        // int notBitmask = ~(bitMask);
+        // int newNumber = notBitmask & n;
+        // System.out.println(newNumber);
         
     }
 
